05c6676650579a195f62c5202d298cb6fdb132d6
GenaDeev <genaroelgenio@hotmail.com>
2024-11-28 20:06:00 -0300

    ✅ Commit inicial

---
+++ js/app.js
@@ -0,0 +1,45 @@
+let montoImpuestos = 0;
+// Cotización constante, en un futuro podria ser un fetch a una api
+const cotizacion = 1000;
+
+// Impuestos al dia de hoy en Argentina
+const impuestos = [
+  {
+    name: "Ganancias",
+    value: 0.3,
+  },
+  {
+    name: "Impuesto Pais",
+    value: 0.3,
+  },
+];
+
+const form = () => {
+  montoImpuestos = 0;
+  alert("Vamos a calcular el precio final con impuestos de tu compra en USD!");
+  const value = parseFloat(prompt("Ingresa el valor de tu compra en USD"));
+  alert(calculate(value, cotizacion));
+};
+
+const calculate = (value, cotizacion) => {
+  if (!value) {
+    alert("Debes ingresar un valor");
+    form();
+  } else {
+    impuestos.forEach((impuesto) => {
+      montoImpuestos += cotizacion * impuesto.value;
+    });
+    const dolarTarjeta = cotizacion + montoImpuestos;
+    const final = value * dolarTarjeta;
+
+    return `
+    El precio final con impuestos es de AR$${final.toFixed(2)}
+    Impuestos:
+    ${impuestos
+      .map((impuesto) => impuesto.name + ": $" + impuesto.value * final)
+      .join(", ")}
+    `;
+  }
+};
+
+form();
